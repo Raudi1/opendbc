328299cfe3150c398fd950f0b1f28f95fd11ddb8
Vrabetz <mpell@web.de>
2025-07-29 22:35:45 +0200

    smaller deadzone

--- opendbc/sunnypilot/car/tesla/virtual_torque_blending.py
+++ opendbc/sunnypilot/car/tesla/virtual_torque_blending.py
@@ -11,7 +11,7 @@ from opendbc.car.interfaces import CarStateBase
 
 TORQUE_TO_ANGLE_MULTIPLIER_OUTER = 4  # Higher = easier to influence when manually steering more than OP
 TORQUE_TO_ANGLE_MULTIPLIER_INNER = 8  # Higher = easier to influence when manually steering less than OP
-TORQUE_TO_ANGLE_DEADZONE = 0.5  # This equates to hands-on level 1, so we don't allow override if not hands-on
+TORQUE_TO_ANGLE_DEADZONE = 0.1  # This equates to hands-on level 1, so we don't allow override if not hands-on
 TORQUE_TO_ANGLE_CLIP = 10.  # Steering (usually) disengages at 2.5 Nm, this limit exists only in case the EPAS gives bad data
 CONTINUED_OVERRIDE_ANGLE = 10.  # The angle difference between OP and user to continue overriding steering (prevents oscillation)
 
